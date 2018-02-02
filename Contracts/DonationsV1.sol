0b2daf7723ee247bb39ce5fe9401fb469320aa43
Sergey <maksimov2135@gmail.com>
2018-02-02 16:29:33 +0700

    Add files via upload
    (#3) Added new map which returns address of wallet by id & number of transaction in this donation

---
+++ Contracts/DonationsV1.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.4.18;
+
+contract Donating {
+    struct Donation {
+        uint id;
+        uint amountOfTransactions;
+        string title;
+        string description;
+        uint currentBalance;
+        uint needToCollect;
+        address walletAuthor;
+        address walletReceiver;
+        uint startingTime;
+        uint endingTime;
+        bool isActive;
+    }
+
+    event Creation(uint donationId);
+
+    uint public currId = 1;    
+    mapping(uint => Donation) public donations;
+    mapping(uint => mapping(address => uint)) public donates;
+    mapping(uint => mapping(uint => address)) public moneysenders;
+    function createDonation(string title, string description, uint needToCollect, 
+        address walletReceiver, uint secondsToVote) 
+    public
+    {
+        require(secondsToVote != 0);
+        uint id = currId++;
+        Donation storage donation = donations[id];
+        donation.id = id;
+        donation.title = title;
+        donation.description = description;
+        donation.currentBalance = 0;
+        donation.needToCollect = needToCollect;
+        donation.walletAuthor = msg.sender;
+        donation.walletReceiver = walletReceiver;
+        donation.startingTime = now;        
+        donation.endingTime = now + secondsToVote;
+        donation.isActive = true;
+        donation.amountOfTransactions = 0;
+        Creation(id);
+    }
+
+    function donate(uint donationId)
+    payable
+    public 
+    {
+        require(donations[donationId].id != 0);
+        require(donations[donationId].isActive);
+        require(donations[donationId].endingTime >= now);        
+        require(msg.sender.balance >= msg.value);
+        donates[donationId][msg.sender] += msg.value;
+        donations[donationId].currentBalance += msg.value;
+        donations[donationId].amountOfTransactions++;
+        moneysenders[donationId][donations[donationId].amountOfTransactions] = msg.sender;
+        if (this.balance >= donations[donationId].needToCollect) {
+            donations[donationId].isActive = false;
+            donations[donationId].walletReceiver.transfer(this.balance);
+        }
+    }
+}
