67e49483692463a0af29a52e3fe5d1799906a09e
Sergey <maksimov2135@gmail.com>
2018-02-02 17:23:00 +0700

    (#3) Map which returns payers doesnt counts twice

--- Contracts/Donating.sol
+++ Contracts/Donating.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.18;
 contract Donating {
     struct Donation {
         uint id;
+        uint amountOfPayers;
         string title;
         string description;
         uint currentBalance;
@@ -19,11 +20,12 @@ contract Donating {
     uint public currId = 1;    
     mapping(uint => Donation) public donations;
     mapping(uint => mapping(address => uint)) public donates;
-
+    mapping(uint => mapping(uint => address)) public moneysenders;
     function createDonation(string title, string description, uint needToCollect, 
-        address walletReceiver, uint startingTime, uint endingTime) 
+        address walletReceiver, uint secondsToVote) 
     public
     {
+        require(secondsToVote != 0);
         uint id = currId++;
         Donation storage donation = donations[id];
         donation.id = id;
@@ -33,22 +35,25 @@ contract Donating {
         donation.needToCollect = needToCollect;
         donation.walletAuthor = msg.sender;
         donation.walletReceiver = walletReceiver;
-        donation.startingTime = startingTime;        
-        donation.endingTime = endingTime;
+        donation.startingTime = now;        
+        donation.endingTime = now + secondsToVote;
         donation.isActive = true;
-
+        donation.amountOfPayers = 0;
         Creation(id);
     }
 
-    function donate(uint donationId, uint currTime)
+    function donate(uint donationId)
     payable
     public 
     {
         require(donations[donationId].id != 0);
         require(donations[donationId].isActive);
-        require(donations[donationId].startingTime <= currTime);
-        require(donations[donationId].endingTime >= currTime);        
+        require(donations[donationId].endingTime >= now);        
         require(msg.sender.balance >= msg.value);
+        if (donates[donationId][msg.sender] == 0) {
+            donations[donationId].amountOfPayers++;
+            moneysenders[donationId][donations[donationId].amountOfPayers] = msg.sender;
+        }
         donates[donationId][msg.sender] += msg.value;
         donations[donationId].currentBalance += msg.value;
         if (this.balance >= donations[donationId].needToCollect) {
