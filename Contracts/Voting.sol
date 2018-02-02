a7d89c7e2eefdcb9714df5b52f820502343984f5
Evgenius2020 <spleen47@mail.ru>
2018-02-02 10:32:06 +0700

    (#3) Fixed returning of new id

--- Contracts/Voting.sol
+++ Contracts/Voting.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.18;
 
 contract Voting {
     struct Votation {
@@ -8,35 +8,39 @@ contract Voting {
         string options;
         uint optionsNumber;
         address walletAuthor;
-        uint begin;
-        uint end;
+        uint startingTime;
+        uint endingTime;
         bool isActive;
     }
 
-    uint currId = 1;    
+    event Creation(uint votationId);
+
+    uint public currId = 1;    
     mapping(uint => Votation) public votations;
     mapping(uint => mapping(address => uint)) public votes;
 
-    function createVotation(string title, string description, string options, uint optionsNumber, uint begin, uint end) 
+    function createVotation(string title, string description, string options,
+        uint optionsNumber, uint startingTime, uint endingTime) 
     public
-    returns (uint) {
+    {
         uint id = currId++;
         Votation storage votation = votations[id];
         votation.id = id;
         votation.title = title;
         votation.description = description;
         votation.walletAuthor = msg.sender;
-        votation.begin = begin;        
-        votation.end = end;
+        votation.startingTime = startingTime;        
+        votation.endingTime = endingTime;
         votation.isActive = true;        
         votation.options = options;
         votation.optionsNumber = optionsNumber;
 
-        return id;
+        Creation(id);
     }
 
     function closeVotation(uint votationId) 
-    public {
+    public 
+    {
         require(votations[votationId].id != 0);
         require(votations[votationId].walletAuthor == msg.sender);
         require(votations[votationId].isActive);
@@ -45,10 +49,12 @@ contract Voting {
     }
 
     function vote(uint votationId, uint optionId, uint currTime) 
-    public {
+    public 
+    {
         require(votations[votationId].id != 0);
         require(votations[votationId].isActive);
-        require(votations[votationId].end >= currTime);
+        require(votations[votationId].startingTime <= currTime);
+        require(votations[votationId].endingTime >= currTime);
         require(optionId != 0 && optionId <= votations[votationId].optionsNumber);
         require(votes[votationId][msg.sender] == 0);
 
